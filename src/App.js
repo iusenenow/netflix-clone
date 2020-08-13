e5eed87833f9db2995d6fc94f215f94a95086927
Wei Zhang <iusenenow@gmail.com>
2020-08-13 21:25:48 +1000

    add banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+
+      {/* Nav */}
+      <Banner />
+
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
