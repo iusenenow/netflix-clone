4a2bc2402a44a2e9b6fd399f3801e68b89ca985c
Wei Zhang <iusenenow@gmail.com>
2020-08-14 13:53:38 +1000

    Updates

---
+++ precache-manifest.bf8e74be795ac43b1be05112cde5219e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7d3876363cd45e119e5956f6ef05ff",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "cbfec22775b33627b1fd",
+    "url": "/netflix-clone/static/css/main.c581651e.chunk.css"
+  },
+  {
+    "revision": "4c7f642d765b9a56dc00",
+    "url": "/netflix-clone/static/js/2.befc7d6e.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.befc7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfec22775b33627b1fd",
+    "url": "/netflix-clone/static/js/main.e7e64569.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
